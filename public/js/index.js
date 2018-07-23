59b6da1ac847dbbe31946986bdabc774b3ee93d7
Kening Ren <rknlh.training@gmail.com>
2018-07-23 19:23:32 -0400

    Create style for the Web GUI
    1, add styles.css
    2, change index.html to use the styles.css
    3, update index.js to:
    3.1, after the customer send his message, message box should be cleared.
    3.2, Button Send-Location should grays out when the location is being
    processed.

--- public/js/index.js
+++ public/js/index.js
@@ -28,11 +28,12 @@ socket.on('newLocationMessage', (message) => {
 jQuery('#message-form').on('submit', (event) => {
   event.preventDefault();
 
+  const messageTextBox = jQuery('[name=message');
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, () => {
-    console.log('Button clicked.');
+    messageTextBox.val('');
   });
 });
 
@@ -41,14 +42,17 @@ locationButton.on('click', () => {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by the browser');
   }
+
+  locationButton.attr('disabled', 'disabled').text('Sending location ...');
   navigator.geolocation.getCurrentPosition((Position) => {
-    console.log(Position);
+    locationButton.removeAttr('disabled').text('Send location') ;
     socket.emit('createLocationMessage', {
       latitude: Position.coords.latitude,
       longitude: Position.coords.longitude,
       timestamp: Position.timestamp
     });
   }, (error) => {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   });
 })
